b4a982a38c6ebece35324b04076fdd365f1bc080
bartoginski <bartoginski@outlook.com>
2021-11-12 12:15:18 +0100

    Add Avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="h-32 w-32 rounded-full my-5"
+    data-aos="flip-up"
+    data-aos-delay="300"
+  >
+    <img class="rounded-full shadow-xl" src="@/assets/img/avatar.jpg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
