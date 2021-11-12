d71aaf4c1afa7013a78a91e27cf30a910b960139
bartoginski <bartoginski@outlook.com>
2021-11-12 14:29:02 +0100

    Add media icons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="h-screen dark:bg-black">
     <ModeBtn />
-    <main class="flex flex-col items-center max-w-6xl m-auto py-5 text-black dark:text-white">
-      <Avatar/>
+    <main
+      class="
+        flex flex-col
+        items-center
+        max-w-6xl
+        m-auto
+        py-5
+        text-black
+        dark:text-white
+      "
+    >
+      <Avatar />
       siemano
+      <SocialIconsVue />
     </main>
   </div>
 </template>
@@ -11,12 +22,16 @@
 <script>
 import ModeBtn from "./components/ModeBtn.vue";
 import Avatar from "./components/Avatar.vue";
+import SocialIconsVue from "./components/SocialIcons.vue";
+
 export default {
   name: "App",
-  components: { ModeBtn, Avatar },
+  components: { ModeBtn, Avatar, SocialIconsVue },
+  data() {
+    return {
+    };
+  },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
